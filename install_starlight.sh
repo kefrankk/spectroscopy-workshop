33f706b237cb8747816180b51fbc1a3b3f692533
kefrankk <ke.heckler95@gmail.com>
2026-07-20 18:58:24 -0300

    chore: add script to install Starlight automatically

---
+++ install_starlight.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Download the package
+wget https://minerva.ufsc.br/starlight/files/downloads/StarlightChains_v04.Distrib.tar.bz2
+
+# Extract
+tar xvfj StarlightChains_v04.Distrib.tar.bz2
+
+# Remove the compiled file
+rm StarlightChains_v04.Distrib.tar.bz2
+
+
+
